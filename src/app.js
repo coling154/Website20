71de39803a61809b49d7f07ca7a857af2eaaa659
Colin Gasiewicz <gasiewca@clarkson.edu>
2024-04-30 15:18:11 -0400

    restructure

--- src/app.js
+++ src/app.js
@@ -5,19 +5,18 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path.join(__dirname, 'src')));
 // Define routes for different pages
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'projects.html'));
+app.get('/projects', (req, res) => {
+    res.sendFile(path.join(__dirname, 'projects.html'));
 });
 
-app.get('/contact', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'blog.html'));
+app.get('/blog', (req, res) => {
+    res.sendFile(path.join(__dirname, 'blog.html'));
 });
 
 // Start the server
